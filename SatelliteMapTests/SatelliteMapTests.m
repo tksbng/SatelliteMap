736f14fd441119843365d0ad9021aabc9d760e9e
Takeshi Bingo <bingotakeshi@Takeshi-no-MacBook-Air.local>
2013-08-31 11:23:12 +0900

    Initial Commit

---
+++ SatelliteMapTests/SatelliteMapTests.m
@@ -0,0 +1,32 @@
+//
+//  SatelliteMapTests.m
+//  SatelliteMapTests
+//
+//  Created by Takeshi Bingo on 2013/08/31.
+//  Copyright (c) 2013年 Takeshi Bingo. All rights reserved.
+//
+
+#import "SatelliteMapTests.h"
+
+@implementation SatelliteMapTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SatelliteMapTests");
+}
+
+@end
